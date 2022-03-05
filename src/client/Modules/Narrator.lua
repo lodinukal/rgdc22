7f97ee384196e5b6968e1ae64b5c2993d4218e3e
socean <lumengeared@gmail.com>
2022-03-05 11:47:33 +0800

    smoothing for pickup

--- src/client/Modules/Narrator.lua
+++ src/client/Modules/Narrator.lua
@@ -4,26 +4,26 @@ local Player = game:GetService("Players").LocalPlayer
 
 local speed = 1
 local amplitude = 2
-local narrator = workspace:WaitForChild("Narrator")
+local narrator = workspace:WaitForChild("Narrator") :: Model
 
 local Narrator = {}
 
 function Narrator.BeginBobbing()
-    local originalCFrame = narrator:GetPrimaryPartCFrame()
-    local phase = 0
-    RunService.Heartbeat:Connect(function(deltaTime)
-        phase += deltaTime * speed
+	local originalCFrame = narrator:GetPrimaryPartCFrame()
+	local phase = 0
+	RunService.Heartbeat:Connect(function(deltaTime)
+		phase += deltaTime * speed
 
-        local cframe = originalCFrame * CFrame.new(0, math.sin(phase) * amplitude, 0)
+		local cframe = originalCFrame * CFrame.new(0, math.sin(phase) * amplitude, 0)
 
-        local character = Player.character
-        if character then
-            local lookAt = CFrame.lookAt(cframe.Position, character:GetPrimaryPartCFrame().Position)
-            narrator:SetPrimaryPartCFrame(lookAt)
-        else
-            narrator:SetPrimaryPartCFrame(cframe)
-        end
-    end)
+		local character = Player.Character
+		if character then
+			local lookAt = CFrame.lookAt(cframe.Position, character:GetPrimaryPartCFrame().Position)
+			narrator:SetPrimaryPartCFrame(lookAt)
+		else
+			narrator:SetPrimaryPartCFrame(cframe)
+		end
+	end)
 end
 
 Narrator.BeginBobbing()
--- src/client/Modules/ThrowModule.lua
+++ src/client/Modules/ThrowModule.lua
@@ -1,67 +1,86 @@
 local ContextActionService = game:GetService("ContextActionService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 
 local camera = workspace.CurrentCamera
+local Common = ReplicatedStorage:WaitForChild("Common")
+
+local Fusion = require(Common:WaitForChild("Fusion"))
 
 local ThrowModule = {}
 ThrowModule.HoldDistance = 30
 ThrowModule.ThrowPower = 50
 ThrowModule.Throwable = {
-    workspace:WaitForChild("Wrench")
+	workspace:WaitForChild("Wrench"),
 }
+ThrowModule.ItemCFrame = Fusion.Value(CFrame.new()) :: Fusion.State<CFrame>
+local ITEM_TWEEN = TweenInfo.new(0.5, Enum.EasingStyle.Quad)
+ThrowModule.SpringItemCFrame = Fusion.Tween(ThrowModule.ItemCFrame, ITEM_TWEEN)
+
+Fusion.Observer(ThrowModule.SpringItemCFrame):onChange(function()
+	if ThrowModule.Item then
+		ThrowModule.Item:SetPrimaryPartCFrame(ThrowModule.SpringItemCFrame:get())
+	end
+end)
 
 function ThrowModule.IsThrowable(part)
-    for i, v in ipairs(ThrowModule.Throwable) do
-        if (not part:IsDescendantOf(v)) then
-            continue
-        end
+	for i, v in ipairs(ThrowModule.Throwable) do
+		if not part:IsDescendantOf(v) then
+			continue
+		end
 
-        return v
-    end
+		return v
+	end
 
-    return nil
+	return nil
 end
 
 function ThrowModule.Hold()
-    local mouseLocation = UserInputService:GetMouseLocation()
-    local ray = camera:ViewportPointToRay(mouseLocation.X, mouseLocation.Y)
+	local mouseLocation = UserInputService:GetMouseLocation()
+	local ray = camera:ViewportPointToRay(mouseLocation.X, mouseLocation.Y)
 
-    local params = RaycastParams.new()
-    params.FilterType = Enum.RaycastFilterType.Blacklist
-    params.FilterDescendantsInstances = {game.Players.LocalPlayer.Character}
+	local params = RaycastParams.new()
+	params.FilterType = Enum.RaycastFilterType.Blacklist
+	params.FilterDescendantsInstances = { game.Players.LocalPlayer.Character }
 
-    local result = workspace:Raycast(ray.Origin, ray.Direction * ThrowModule.HoldDistance, params)
-    if (not result) then
-        warn("nil")
-        return
-    end
-    local Item = ThrowModule.IsThrowable(result.Instance)
-    if (not Item) then
-        warn("not a throwable")
-        return
-    end
-    Item.PrimaryPart.Anchored = true
-    ThrowModule.Item = Item
-    ThrowModule.HoldConnection = RunService.Heartbeat:Connect(function(deltaTime)
-        ThrowModule.Item:SetPrimaryPartCFrame(camera.CFrame + camera.CFrame.LookVector * 5)
-    end)
+	local result = workspace:Raycast(ray.Origin, ray.Direction * ThrowModule.HoldDistance, params)
+	if not result then
+		warn("nil")
+		return
+	end
+	local Item = ThrowModule.IsThrowable(result.Instance)
+	if not Item then
+		warn("not a throwable")
+		return
+	end
+	Item.PrimaryPart.Anchored = true
+	ThrowModule.Item = Item
+	ThrowModule.HoldConnection = RunService.Heartbeat:Connect(function(deltaTime)
+		ThrowModule.ItemCFrame:set(camera.CFrame + camera.CFrame.LookVector * 5)
+	end)
 end
 
 function ThrowModule.LetGo()
-    ThrowModule.HoldConnection:Disconnect()
-    ThrowModule.Item.PrimaryPart.Anchored = false
-    ThrowModule.Item.PrimaryPart:ApplyImpulse(camera.CFrame.LookVector * ThrowModule.ThrowPower)
+	if ThrowModule.HoldConnection then
+		ThrowModule.HoldConnection:Disconnect()
+	end
+	local item = ThrowModule.Item
+	if item then
+		ThrowModule.Item = nil
+		item.PrimaryPart.Anchored = false
+		item.PrimaryPart:ApplyImpulse(camera.CFrame.LookVector * ThrowModule.ThrowPower)
+	end
 end
 
 ContextActionService:BindAction("HoldItem", function(actionName, inputState, inputObj)
-    if inputState == Enum.UserInputState.Begin then
-        print("Held")
-        ThrowModule.Hold()
-    elseif inputState == Enum.UserInputState.End then
-        print("Let go")
-        ThrowModule.LetGo()
-    end
+	if inputState == Enum.UserInputState.Begin then
+		print("Held")
+		ThrowModule.Hold()
+	elseif inputState == Enum.UserInputState.End then
+		print("Let go")
+		ThrowModule.LetGo()
+	end
 end, false, Enum.UserInputType.MouseButton1)
 
 return ThrowModule
