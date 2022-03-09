b52e377b4de02da3415ceaeed35238d3d86ec377
Hex <lumengeared@gmail.com>
2022-03-09 19:25:00 +0800

    LaserModule changes

--- src/client/Modules/LaserModule.lua
+++ src/client/Modules/LaserModule.lua
@@ -1,14 +1,17 @@
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
 local ContextActionService = game:GetService("ContextActionService")
+local CollectionService = game:GetService("CollectionService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local Mirrors = workspace:WaitForChild("Mirrors")
+local PartCache = require(ReplicatedStorage:WaitForChild("Common"):WaitForChild("PartCache"))
 
-local Start = Mirrors:WaitForChild("Start")
+local MIRROR_TAG = "PuzzleMirror"
+local SOURCE_TAG = "PuzzleLaserSource"
 
-local LaserSound = script:WaitForChild("LaserSound")
-local Beam = script:WaitForChild("Beam")
-local BurnEffect = script:WaitForChild("BurnEffect")
+-- local LaserSound = script:WaitForChild("LaserSound")
+-- local Beam = script:WaitForChild("Beam")
+-- local BurnEffect = script:WaitForChild("BurnEffect")
 
 local Player = game.Players.LocalPlayer
 
@@ -16,66 +19,61 @@ local Mouse = Player:GetMouse()
 
 local LaserModule = {}
 LaserModule.Radius = 0.5
-LaserModule.Lasers = {}
+LaserModule.ReflectLimit = 20
+LaserModule.Distance = 100
 
-function LaserModule:ShowBeam(origin, direction)
-    local part = Instance.new("Part")
-    local sound = LaserSound:Clone()
-    sound:Play()
-    sound.Parent = part
+LaserModule.CachedMirrors = {}
+LaserModule.CachedSources = {}
+LaserModule.ActiveLasers = {}
 
-    part.Name = "Laser"
-    part.Anchored = true
-    
-    part.Material = Enum.Material.Neon
-    part.Transparency = 1
-    
-    part.CFrame = CFrame.lookAt(origin + direction/2 , origin)
-    part.Size = Vector3.new(self.Radius, self.Radius, direction.Magnitude)
+local beamTemplate do
+    beamTemplate = Instance.new("Part")
+    -- local sound = LaserSound:Clone()
+    -- sound:Play()
+    -- sound.Parent = part
+
+    beamTemplate.Name = "Laser"
+    beamTemplate.Anchored = true
+
+    beamTemplate.Material = Enum.Material.Neon
+    beamTemplate.Transparency = 0.2
+
+    -- part.CFrame = CFrame.lookAt(origin + direction/2 , origin)
+    -- part.Size = Vector3.new(self.Radius, self.Radius, direction.Magnitude)
 
     local attachment0 = Instance.new("Attachment")
     attachment0.Name = "Attachment0"
-    attachment0.Parent = part
-    attachment0.WorldPosition = origin
+    attachment0.Parent = beamTemplate
+    -- attachment0.WorldPosition = origin
 
     local attachment1 = Instance.new("Attachment")
     attachment1.Name = "Attachment1"
-    attachment1.Parent = part
-    attachment1.WorldPosition = origin + direction
+    attachment1.Parent = beamTemplate
+    -- attachment1.WorldPosition = origin + direction
 
-    local burnEffect = BurnEffect:Clone()
-    burnEffect.Parent = attachment1
+    -- local burnEffect = BurnEffect:Clone()
+    -- burnEffect.Parent = attachment1
 
-    local beam = Beam:Clone()
-    beam.Attachment0 = attachment0
-    beam.Attachment1 = attachment1
-	beam.Parent = part
-	
-    part.CanCollide = false
-	part.Parent = workspace
-    
-    table.insert(self.Lasers, part)
+    -- local beam = Beam:Clone()
+    -- beam.Attachment0 = attachment0
+    -- beam.Attachment1 = attachment1
+	-- beam.Parent = part
 
-    return part
+    beamTemplate.CanCollide = false
+	beamTemplate.Parent = workspace
 end
+
+LaserModule.LaserCache = PartCache.new(beamTemplate, 1, workspace)
 
 local function reflect(lv, nv)
     return lv - 2 * nv * (lv:Dot(nv))
 end
 
-function LaserModule:IsMirror(part)
-    for i, v in ipairs(Mirrors:GetChildren()) do
-        if v ~= part then
-            continue
-        end
-
-        return true
-    end
-
-    return false
+function LaserModule:IsMirror(part: Instance)
+    return CollectionService:HasTag(part, MIRROR_TAG)
 end
 
-local increment = 5
+local increment = 1
 
 function round(number)
 	return math.floor((number / increment) + 0.5) * increment
@@ -83,19 +81,78 @@ end
 
 function AngleFromAxis(axis, r)
 	local relativeAngle = math.rad(round(math.deg(r)))
-	
+
 	return axis == Enum.Axis.X and {relativeAngle, 0, 0} 
 		or axis == Enum.Axis.Y and {0, relativeAngle, 0} 
 		or axis == Enum.Axis.Z and {0, 0, relativeAngle}
 end
 
 function LaserModule:Clear()
-    for i, v in ipairs(self.Lasers) do
-        v:Destroy()
+    for _, laser in pairs(self.ActiveLasers) do
+        self.LaserCache:ReturnPart(laser)
+    end
+    self.ActiveLasers = {}
+end
+
+function LaserModule:ShowBeam(origin: Vector3, direction: Vector3)
+    local laser = self.LaserCache:GetPart()
+    laser.CFrame = CFrame.lookAt(origin + direction/2 , origin)
+    laser.Size = Vector3.new(self.Radius, self.Radius, direction.Magnitude)
+
+    laser.Attachment0.WorldPosition = origin
+    laser.Attachment1.WorldPosition = origin + direction
+
+    table.insert(self.ActiveLasers, laser)
+
+    return laser
+end
+
+function LaserModule:MirrorSet(mirror: Instance, mirrorConfine: string?)
+    mirrorConfine = if mirrorConfine then mirrorConfine else true
+    self.CachedMirrors[mirrorConfine] = if self.CachedMirrors[mirrorConfine] then self.CachedMirrors[mirrorConfine] else {}
+    table.insert(self.CachedMirrors[mirrorConfine], mirror)
+end
+
+function LaserModule:DoLaserSim(source: Instance)
+    local direction = source.CFrame.LookVector * self.Distance
+    local origin = source.Position
+    local part = source
+
+    for _ = 1, self.ReflectLimit do
+
+        local raycastParams = RaycastParams.new()
+        raycastParams.FilterDescendantsInstances = {self.ActiveLasers, self.CachedSources}
+        raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+
+        local result = workspace:Raycast(origin, direction, raycastParams)
+        local laser = self:ShowBeam(origin, result and result.Position - origin or direction)
+
+        if not result or not self:IsMirror(result.Instance) then return end
+
+        local lightVector = (result.Position - laser.Position).Unit 
+        local reflectionVector = reflect(lightVector, result.Normal)
+
+        part = result.Instance
+        origin = result.Position 
+        direction = reflectionVector * self.Distance
     end
 end
 
 function LaserModule:Start()
+
+    for _, mirror in ipairs(CollectionService:GetTagged(MIRROR_TAG)) do
+        self:MirrorSet(mirror, mirror:GetAttribute("SourceConfine"))
+    end
+    CollectionService:GetInstanceAddedSignal(MIRROR_TAG):Connect(function(mirror: Instance)
+        self:MirrorSet(mirror, mirror:GetAttribute("SourceConfine"))
+    end)
+
+    for _, source in ipairs(CollectionService:GetTagged(SOURCE_TAG)) do
+        self.CachedSources[source.Name] = source
+    end
+    CollectionService:GetInstanceAddedSignal(SOURCE_TAG):Connect(function(source: Instance)
+        self.CachedSources[source.Name] = source
+    end)
 
     local arcHandles
     ContextActionService:BindAction("SelectMirror", function(actionName, inputState, inputObject)
@@ -129,33 +186,13 @@ function LaserModule:Start()
                 lastCFrame = target.CFrame
             end)
         end
-    end, false, Enum.UserInputType.MouseButton1)
+    end, false, Enum.UserInputType.MouseButton1, Enum.UserInputType.Touch)
 
     RunService.Heartbeat:Connect(function(deltaTime)
         self:Clear()
 
-        local direction = Start.CFrame.LookVector * 50
-        local origin = Start.Position 
-        local part = Start
-        
-        for reflections = 1, #Mirrors:GetChildren() do
-            print(self.Lasers)
-
-            local raycastParams = RaycastParams.new()
-            raycastParams.FilterDescendantsInstances = {self.Lasers, part}
-            raycastParams.FilterType = Enum.RaycastFilterType.Blacklist 
-
-            local result = workspace:Raycast(origin, direction, raycastParams)
-            local laser = self:ShowBeam(origin, result and result.Position - origin or direction)
-            
-            if not result then break end 
-            
-            local lightVector = (result.Position - laser.Position).Unit 
-            local reflectionVector = reflect(lightVector, result.Normal)
-            
-            part = result.Instance
-            origin = result.Position 
-            direction = reflectionVector * 50
+        for _, source in pairs(self.CachedSources) do
+            self:DoLaserSim(source)
         end
     end)
 
