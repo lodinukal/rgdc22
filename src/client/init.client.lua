f409112bde5c37ca123a428de67af8637ca6218d
ghosty <84999408+ghosty69420@users.noreply.github.com>
2022-03-10 17:01:41 +0000

    Removed WindLines.lua (obsolete)

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -10,7 +10,6 @@ local Fusion = require(Common:WaitForChild("fusion"))
 local Modules = script:WaitForChild("Modules")
 local Gui = script:WaitForChild("Gui")
 
-local WindLines = require(Modules:WaitForChild("WindLines"))
 local CameraModule = require(Modules:WaitForChild("CameraModule"))
 local PhysicsModule = require(Modules:WaitForChild("Physics"))
 PhysicsModule:Start()
@@ -19,13 +18,6 @@ local DeathScreen = require(Gui:WaitForChild("DeathScreen"))
 local PhysicsHighLight = require(Gui:WaitForChild("PhysicsHighLight"))
 
 local Player = game.Players.LocalPlayer
-
-WindLines:Init({
-	Direction = Vector3.new(1, 0, 0.3),
-	Speed = 20,
-	Lifetime = 1.5,
-	SpawnRate = 11,
-})
 
 local e = workspace:WaitForChild("scale")
 
