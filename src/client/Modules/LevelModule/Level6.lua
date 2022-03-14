dc4552816fe1f82fc4822a132658f1eb5507822b
Hex <lumengeared@gmail.com>
2022-03-14 16:09:19 +0800

    templated new Levels

---
+++ src/client/Modules/LevelModule/Level6.lua
@@ -0,0 +1,41 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Levels = ReplicatedStorage:WaitForChild("Levels")
+
+local Player = game.Players.LocalPlayer
+
+local PlayerScripts = Player:WaitForChild("PlayerScripts")
+
+local Client = PlayerScripts:WaitForChild("Client")
+
+local Modules = Client:WaitForChild("Modules")
+
+local Spider = require(Modules:WaitForChild("Spider"))
+
+local Data = {
+	Folder = Levels:WaitForChild("Level2"),
+}
+
+local Enemies = {}
+
+local function OnLoaded(self, map)
+	for i, v in ipairs(map.enemies:GetChildren()) do
+		local spider = Spider.new(v)
+		spider:init()
+	end
+end
+
+local function OnUnloaded(self, map)
+	
+end
+
+local function CanProceed()
+	return (#Enemies == 0)
+end
+
+return {
+	Data = Data,
+	OnLoaded = OnLoaded,
+	OnUnloaded = OnUnloaded,
+	CanProceed = CanProceed
+}
